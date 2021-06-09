9d7d13b53b6ccc453035db9e2e4a8c00600bc738
Santhosh Varathan <sanvsam11@gmail.com>
2021-06-09 18:48:05 +0530

    firebase first

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const path = require('path')
 app.get('/hello', (req, res)=>{
     res.send("Hello World")
 })
-app.use(express.static(path.resolve(__dirname, '../client/build')))
-app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-})
+// app.use(express.static(path.resolve(__dirname, '../client/build')))
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+// })
+app.use(express.static(path.join(__dirname, "..", "build")))
+app.use(express.static("public"))
 app.listen(5000, ()=>{
     console.log("started")
 })
